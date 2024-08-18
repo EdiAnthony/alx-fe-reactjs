9c8c5d4ead036afbc4927689b5758a9cfd527f1f
Edi Anthony <anthmashok@gmail.com>
2024-08-18 01:00:20 -0700

    header, main content, footer components added

---
+++ alx-react-app/src/components/MainContent.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function MainContent() {
+    return (
+        <main>
+            <p>Here is a list of my favorite cities:</p>
+            <ul>
+                <li>Lagos</li>
+                <li>Canada</li>
+                <li>Tokyo</li>
+            </ul>
+        </main>
+    );
+}
+
+export default MainContent;
