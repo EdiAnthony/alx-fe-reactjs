271109b6fed37080bf60a464ccdf344cb9ec8eb7
Edi Anthony <anthmashok@gmail.com>
2024-09-15 00:33:19 -0700

    Attractive displays accross different screen sizes implemented

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,11 +1,11 @@
 function UserProfile() {
-    return (
-      <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
-        <img src="https://via.placeholder.com/150" alt="User" className="w-36 h-36 rounded-full mx-auto" />
-        <h1 className="text-xl text-blue-800 my-4">Anthony</h1>
-        <p className="text-gray-600 text-base">Developer at Skytech Computer Entreprise.  I love to write code and explore new technologies.</p>
-      </div>
-    );
-  }
-  
-  export default UserProfile;
+  return (
+    <div className="user-profile bg-gray-100 p-4 sm:p-8 md:max-w-sm lg:max-w-md mx-auto my-20 rounded-lg shadow-lg">
+      <img src="https://via.placeholder.com/150" alt="User" className="w-24 h-24 sm:w-36 sm:h-36 rounded-full mx-auto" />
+      <h1 className="text-lg sm:text-xl md:text-2xl text-blue-800 my-4">John Anthony</h1>
+      <p className="text-sm sm:text-base md:text-lg text-gray-600">Developer at Skytech Computer Enterprise. Loves to write code and explore new technologies.</p>
+    </div>
+  );
+}
+
+export default UserProfile;
